6a8110373829fffde56c1bed86edca9bf5046a00
Fruxz <cedricspitzer@outlook.de>
2020-08-08 01:30:26 +0200

    Basic structure init

---
+++ src/main/java/de/fruxz/farmengine/Main.kt
@@ -0,0 +1,19 @@
+package de.fruxz.farmengine
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onLoad() {
+        super.onLoad()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+}
