6bf0faeb1873f987a834a72bdf19e39369dcad05
Fruxz <cedricspitzer@outlook.de>
2020-08-08 02:40:25 +0200

    No World generation issue fixed!

--- src/main/java/de/fruxz/farmengine/Main.kt
+++ src/main/java/de/fruxz/farmengine/Main.kt
@@ -24,7 +24,7 @@ class Main : JavaPlugin() {
 
         getCommand("farmworld")!!.setExecutor(FarmWorldCommand())
 
-        if (Bukkit.getWorld(farmworldName) != null) {
+        if (Bukkit.getWorld(farmworldName) == null) {
 
             val creator = WorldCreator(farmworldName)
 
