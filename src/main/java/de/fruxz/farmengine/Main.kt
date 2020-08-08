9434f28a72b889afeace6d9fa2c280a4614bea0b
Fruxz <cedricspitzer@outlook.de>
2020-08-08 02:02:11 +0200

    Added FarmWorld generation

--- src/main/java/de/fruxz/farmengine/Main.kt
+++ src/main/java/de/fruxz/farmengine/Main.kt
@@ -1,15 +1,48 @@
 package de.fruxz.farmengine
 
+import org.bukkit.Bukkit
+import org.bukkit.World
+import org.bukkit.WorldCreator
+import org.bukkit.WorldType
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 
 class Main : JavaPlugin() {
+
+    companion object {
+        @JvmStatic
+        var farmworldExists: Boolean = false
+    }
 
     override fun onLoad() {
         super.onLoad()
     }
 
     override fun onEnable() {
-        super.onEnable()
+        val farmworldName = "FarmENGINE"
+
+        if (Bukkit.getWorld(farmworldName) != null) {
+
+            val creator = WorldCreator(farmworldName)
+
+            creator.environment(World.Environment.NORMAL)
+            creator.type(WorldType.LARGE_BIOMES)
+
+            Bukkit.getConsoleSender().sendMessage("§8>> §cFarmENGINE is creating farmworld...")
+
+            object : BukkitRunnable() {
+                override fun run() {
+                    creator.createWorld()
+                }
+            }.runTask(this)
+
+            Bukkit.getConsoleSender().sendMessage("§8>> §aFarmENGINE world creation finished!")
+
+            farmworldExists = true
+
+        } else
+            farmworldExists = true
+
     }
 
     override fun onDisable() {
